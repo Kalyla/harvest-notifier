3284569b9326e15402a6e849186cc2fd887089ae
Kakyla <52607322+Kalyla@users.noreply.github.com>
2024-09-04 15:23:29 +0300

    Update daily.js

--- daily.js
+++ daily.js
@@ -202,23 +202,23 @@ async function slackNotify(usersToNotify, timeSheetDateToCheck) {
         ],
       },
     ];
-  //    const response = await fetch(
-  //      `https://slack.com/api/chat.postMessage?channel=${
-  //        process.env.SLACK_CHANNEL
-  //      }&blocks=${encodeURIComponent(JSON.stringify(slackBlocks))}&pretty=1`,
-  //      {
-  //        method: 'post',
-  //        headers: {
-  //          'Content-Type': 'application/x-www-form-urlencoded',
-  //          Accept: 'application/json',
-  //          charset: 'utf-8',
-  //          Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
-  //        },
-  //      }
-  //    );
-  //   const data = await response.json();
-  //   console.log('slackResponse', data);
-  // } else return;
+     const response = await fetch(
+       `https://slack.com/api/chat.postMessage?channel=${
+         process.env.SLACK_CHANNEL
+       }&blocks=${encodeURIComponent(JSON.stringify(slackBlocks))}&pretty=1`,
+       {
+         method: 'post',
+         headers: {
+           'Content-Type': 'application/x-www-form-urlencoded',
+           Accept: 'application/json',
+           charset: 'utf-8',
+           Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
+         },
+       }
+     );
+    // const data = await response.json();
+    console.log('slackResponse', data);
+  } else return;
 }
 
 async function app() {
